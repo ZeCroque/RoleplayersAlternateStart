9f361517ea6ed60aab3a0689dad3988b80b304cb
ZeCroque <zobilezeub@gmail.com>
2026-09-26 14:17:12 +0200

    Updated CIScripts to get new factorize mo2 shortcut scripts Cleared archives when localizing to make sure they are not loaded by xTranslator Launched xTranslator after having localized the .esp

--- build.py
+++ build.py
@@ -2,11 +2,30 @@ from CIScripts import translator
 from CIScripts import ba2_creator
 from CIScripts import utils
 from CIScripts import mod_info
+from CIScripts import mo2
 import os
 
+def ClearArchives(isAF):
+    archiveName = mod_info.config.mainArchiveNameAF if isAF else mod_info.config.mainArchiveName
+
+    fullArchivePath = "./Data/" + archiveName + mod_info.config.archiveExtension
+    if os.path.isfile(fullArchivePath):
+        os.remove(fullArchivePath)
+
+    fullArchivePath = "./Data/" + archiveName + "_xbox" + mod_info.config.archiveExtension
+    if os.path.isfile(fullArchivePath):
+        os.remove(fullArchivePath)
+
+    fullArchivePath = "./Data/" + archiveName + "_ps" + mod_info.config.archiveExtension
+    if os.path.isfile(fullArchivePath):
+        os.remove(fullArchivePath)
+    
 def main():   
     if utils.AskForUserConfirm("Would you like to localize the .esm?"):
         translator.Translate()
+        ClearArchives(True)
+        ClearArchives(False)
+        mo2.runMO2Target("xTranslator")
         if(not utils.AskForUserConfirm(".esm localized. Proceed to archive creation?")):
             return
 
