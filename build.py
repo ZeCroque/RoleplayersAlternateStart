8fff2159b1903738a526216763676e0cc8fb2e75
ZeCroque <zobilezeub@gmail.com>
2026-09-26 16:35:10 +0200

    Updated CIScripts to get the modularized version of the build script Replaced build script usages

--- build.py
+++ build.py
@@ -1,36 +1,7 @@
-from CIScripts import translator
-from CIScripts import ba2_creator
-from CIScripts import utils
-from CIScripts import mod_info
-from CIScripts import mo2
-import os
+from CIScripts import package
 
-def ClearArchives(isAF):
-    archiveName = mod_info.config.mainArchiveNameAF if isAF else mod_info.config.mainArchiveName
-
-    fullArchivePath = "./Data/" + archiveName + mod_info.config.archiveExtension
-    if os.path.isfile(fullArchivePath):
-        os.remove(fullArchivePath)
-
-    fullArchivePath = "./Data/" + archiveName + "_xbox" + mod_info.config.archiveExtension
-    if os.path.isfile(fullArchivePath):
-        os.remove(fullArchivePath)
-
-    fullArchivePath = "./Data/" + archiveName + "_ps" + mod_info.config.archiveExtension
-    if os.path.isfile(fullArchivePath):
-        os.remove(fullArchivePath)
-    
 def main():   
-    if utils.AskForUserConfirm("Would you like to localize the .esm?"):
-        translator.Translate()
-        ClearArchives(True)
-        ClearArchives(False)
-        mo2.runMO2Target("xTranslator")
-        if(not utils.AskForUserConfirm(".esm localized. Proceed to archive creation?")):
-            return
-
-    if(os.path.isfile(mod_info.config.modShortName + "_Thumbnail.png") or utils.AskForUserConfirm("Thumbnail file not found. Proceed anyway?")):
-        ba2_creator.CreateArchives()
+    package.Package()
 
 if __name__ == "__main__":
     main()
